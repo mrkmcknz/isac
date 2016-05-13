c14d82e2710aa3cd4c83f1a3931929f49226bca0
Mark McKenzie <mrkmcknz@gmail.com>
2016-05-13 14:10:19 +0100

    Fixed missing packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="isac",
     version="0.1.0",
     description=("Intent extraction engine."),
     license=("LGPL-3"),
-    packages=["isac", "isac.utils"],
+    packages=find_packages(exclude=[]),
     install_requires=[
         "textblob==0.11.1",
         "six==1.10.0"
